c19ca8800ec167d5afcf6b6cc88b039ecb102c60
mohamad-kareem <mohamadkareemeng@gmail.com>
2026-03-07 11:39:06 +0100

    ERORR

--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const response = NextResponse.redirect(new URL("/login", request.url));
+  const response = NextResponse.redirect(new URL("/login", request.url), 303);
 
   response.cookies.set("token", "", {
     httpOnly: true,
